507c69e5bbbed48185769caac29a4fda80c9eae0
Michael Møller-Hansen <michael.moeller84@gmail.com>
2019-03-06 20:01:10 +0100

    SeparationEventTests init

--- AirTrafficMonitor/AirTrafficMonitor.Lib/Models/SeparationEvent.cs
+++ AirTrafficMonitor/AirTrafficMonitor.Lib/Models/SeparationEvent.cs
@@ -8,5 +8,12 @@ namespace AirTrafficMonitor.Lib.Models
         public string Tag1 { get; set; }
         public string Tag2 { get; set; }
         public DateTime Timestamp { get; set; }
+
+        public SeparationEvent(string tag1, string tag2, DateTime timestamp)
+        {
+            Tag1 = tag1;
+            Tag2 = tag2;
+            Timestamp = timestamp;
+        }
     }
 }
