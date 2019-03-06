978beeac8632fa96ee2ba5ebb8fee92457405dc3
Mads Skytte Nielsen <au536099@uni.au.dk>
2019-03-06 19:58:41 +0100

    Added files to test

---
+++ AirTrafficMonitor/AirTrafficMonitor.Lib.UnitTests/EventArgs/SeparationEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using AirTrafficMonitor.Lib.Interfaces;
+using NUnit;
+using NUnit.Framework;
+
+namespace AirTrafficMonitor.Lib.UnitTests.EventArgs
+{
+    [TestFixture]
+    public class SeparationEventArgsTest
+    {
+
+    }
+}
---
+++ AirTrafficMonitor/AirTrafficMonitor.Lib.UnitTests/EventArgs/TrackEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitor.Lib.Interfaces;
+using NUnit.Framework;
+
+namespace AirTrafficMonitor.Lib.UnitTests.EventArgs
+{
+    [TestFixture]
+    public class TrackEventArgsTest
+    {
+
+    }
+}
