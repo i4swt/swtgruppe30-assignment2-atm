2a1911d0125c5eaa480538df706c960d6e92fe32
Michael Møller-Hansen <michael.moeller84@gmail.com>
2019-03-06 16:13:07 +0100

    Added AirspaceService

---
+++ AirTrafficMonitor/AirTrafficMonitor.Lib/Services/AirspaceService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using AirTrafficMonitor.Lib.Interfaces;
+
+namespace AirTrafficMonitor.Lib.Services
+{
+    public class AirspaceService : IAirspaceService
+    {
+        public HashSet<ITrack> GetTrackingsInAirspace(HashSet<ITrack> trackings, IAirspace airspace)
+        {
+
+
+
+
+
+
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
