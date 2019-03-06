c8e3d1eb6712b6bb32bc3a8f5353b52ef71db8a3
Mads Skytte Nielsen <au536099@uni.au.dk>
2019-03-06 20:06:13 +0100

    Added constructor test for both SeparationEventArgs and TrackEventArgs.

--- AirTrafficMonitor/AirTrafficMonitor.Lib.UnitTests/EventArgs/TrackEventArgs.cs
+++ AirTrafficMonitor/AirTrafficMonitor.Lib.UnitTests/EventArgs/TrackEventArgs.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AirTrafficMonitor.Lib.EventArgs;
 using AirTrafficMonitor.Lib.Interfaces;
 using NUnit.Framework;
 
@@ -11,6 +12,12 @@ namespace AirTrafficMonitor.Lib.UnitTests.EventArgs
     [TestFixture]
     public class TrackEventArgsTest
     {
+        public void Ctor_TestGetMethod_ContainsSameAsInserted()
+        {
+            HashSet<ITrack> toInsertIntoTrackEventArgs = new HashSet<ITrack>();
+            TrackEventArgs uut = new TrackEventArgs(toInsertIntoTrackEventArgs);
 
+            Assert.That(uut.Trackings, Is.EqualTo(toInsertIntoTrackEventArgs));
+        }
     }
 }
