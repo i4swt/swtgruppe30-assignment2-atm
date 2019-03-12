3000fb439940e4ee6ece84bad7557126b7c6aae1
Jesper <201707587@post.au.dk>
2019-03-12 15:52:35 +0100

    Loggingservice done, beginning tests

--- AirTrafficMonitor/AirTrafficMonitor.Lib/Services/LoggingService.cs
+++ AirTrafficMonitor/AirTrafficMonitor.Lib/Services/LoggingService.cs
@@ -1,12 +1,24 @@
-﻿using AirTrafficMonitor.Lib.Interfaces;
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using AirTrafficMonitor.Lib.Interfaces;
 
 namespace AirTrafficMonitor.Lib.Services
 {
     public class LoggingService : ILoggingService
     {
+        private readonly string filePath = "Log.txt";
+
         public void Log(string entry)
         {
-            throw new System.NotImplementedException();
+            if(entry==null) throw new ArgumentNullException();
+
+            if (entry == "") return;
+
+            using (StreamWriter sw = File.AppendText(filePath))
+            {
+                sw.WriteLine(entry);
+            }
         }
     }
 }
